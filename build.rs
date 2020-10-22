5f6631c838d316702ddafd85ac5d0beb87d459c1
Jacek Chmielewski <jchmielewski@teonite.com>
2020-10-22 12:25:04 +0200

    Cleanup: remove example code.

--- build.rs
+++ build.rs
@@ -1,6 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // compiling protos using path on build time
-    tonic_build::compile_protos("proto/say.proto")?;
     tonic_build::compile_protos("proto/wgservice.proto")?;
     Ok(())
 }
--- src/hello.rs
+++
@@ -1,2 +0,0 @@
-// this would include code generated for package hello from .proto file
-tonic::include_proto!("hello");
