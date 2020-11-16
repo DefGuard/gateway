b4a6d7cad8b2ad240423cfd5225d9dd037d5abd0
Jacek Chmielewski <jchmielewski@teonite.com>
2020-11-16 10:39:10 +0100

    Add protos as git submodule.

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // compiling protos using path on build time
-    tonic_build::compile_protos("proto/wgservice.proto")?;
+    tonic_build::compile_protos("proto/wireguard/wireguard_service.proto")?;
     Ok(())
 }
