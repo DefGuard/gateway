e7c335f4131e106ee56f6d24f2784740f52970b3
Maciek <mwojcik@teonite.com>
2023-04-06 09:20:21 +0200

    use builtin clap command (#15)

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 use clap::Parser;
-use defguard_gateway::{gateway::start, Config, VERSION};
+use defguard_gateway::{gateway::start, Config};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config = Config::parse();
-    if config.version {
-        println!("{}", VERSION);
-    } else {
-        start(&config).await?;
-    }
+    start(&config).await?;
     Ok(())
 }
