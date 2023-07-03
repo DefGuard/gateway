4c6ae4b75e15a025e005cb93bdfda7e0eea36a33
Maciej Wójcik <wojcik91@gmail.com>
2023-07-03 23:41:13 +0200

    fix build script

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // compiling protos using path on build time
-    tonic_build::compile_protos("proto/wireguard/gateway.proto")?;
+    let mut config = prost_build::Config::new();
+    config.protoc_arg("--experimental_allow_proto3_optional");
+    tonic_build::configure().compile_with_config(
+        config,
+        &["proto/wireguard/gateway.proto"],
+        &["proto/wireguard"],
+    )?;
     Ok(())
 }
