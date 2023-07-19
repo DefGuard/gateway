f53396b27bd5a663f5c3fddcbdb84d690614bb59
Maciej Wójcik <wojcik91@gmail.com>
2023-07-19 19:56:44 +0200

    refactor gateway to use a struct

--- src/gateway.rs
+++ src/gateway.rs
@@ -1,26 +1,19 @@
 use crate::mask;
 use crate::proto::ConfigurationRequest;
 #[cfg(target_os = "linux")]
-use crate::wireguard::netlink::delete_interface;
 use crate::{
     config::Config,
     error::GatewayError,
     proto::{gateway_service_client::GatewayServiceClient, update, Configuration, Update},
-    wireguard::{setup_interface, wgapi::WGApi},
+    wireguard::{netlink::delete_interface, setup_interface, wgapi::WGApi},
     VERSION,
 };
-use env_logger::{init_from_env, Env, DEFAULT_FILTER_ENV};
 use gethostname::gethostname;
 use lazy_static::lazy_static;
 use std::{
-    fs::File,
-    io::Write,
-    process,
-    str::FromStr,
     sync::Arc,
     time::{Duration, SystemTime},
 };
-use syslog::{BasicLogger, Facility, Formatter3164};
 use tokio::{sync::Mutex, time::sleep};
 use tonic::{
     codegen::InterceptedService,
@@ -35,217 +28,199 @@ lazy_static! {
         .expect("Unable to get current hostname");
 }
 
-/// Starts tokio thread collecting stats and sending them to backend service via gRPC.
-fn spawn_stats_thread(
-    client: Arc<
-        Mutex<
-            GatewayServiceClient<
-                InterceptedService<
-                    Channel,
-                    impl Fn(Request<()>) -> Result<Request<()>, Status> + Send + 'static,
-                >,
-            >,
-        >,
-    >,
-    period: Duration,
-    ifname: String,
-    userspace: bool,
-) {
-    // Create an async stream that periodically yields wireguard interface statistics.
-    info!("Spawning stats thread");
-    let stats_stream = async_stream::stream! {
-        let api = WGApi::new(ifname, userspace);
-        loop {
-            debug!("Sending peer stats update");
-            match api.read_host() {
-                Ok(host) => {
-                    for peer in host
-                        .peers
-                        .values()
-                        .filter(|p| p.last_handshake.map_or(
-                            false,
-                            |lhs| lhs != SystemTime::UNIX_EPOCH)
-                        ) {
-                        yield peer.into();
-                    }
-                },
-                Err(err) => error!("Failed to retrieve WireGuard interface stats {}", err),
-            }
-            sleep(period).await;
-        }
-    };
-    // Spawn the thread
-    tokio::spawn(async move {
-        let mut client = client.lock().await;
-        let _r = client.stats(Request::new(stats_stream)).await;
-    });
+pub struct Gateway {
+    config: Config,
 }
 
-/// Performs complete interface reconfiguration based on `configuration` object.
-/// Called when gateway (re)connects to GRPC endpoint and retrieves complete
-/// network and peers data.
-fn configure(config: &Config, configuration: Configuration) -> Result<(), GatewayError> {
-    debug!(
-        "Received configuration, reconfiguring WireGuard interface: {:?}",
-        mask!(configuration, prvkey)
-    );
-    if !config.userspace {
-        #[cfg(target_os = "linux")]
-        let _ = delete_interface(&config.ifname);
+impl Gateway {
+    pub fn new(config: Config) -> Result<Self, GatewayError> {
+        Ok(Self { config })
     }
-    setup_interface(&config.ifname, config.userspace, &configuration)?;
-    info!(
-        "Reconfigured WireGuard interface: {:?}",
-        mask!(configuration, prvkey)
-    );
-    Ok(())
-}
 
-/// Continuously tries to connect to GRPC endpoint. Once the connection is established
-/// configures the interface, starts the stats thread, connects and returns the updates stream
-async fn connect(
-    config: &Config,
-    client: Arc<
-        Mutex<
-            GatewayServiceClient<
-                InterceptedService<
-                    Channel,
-                    impl Fn(Request<()>) -> Result<Request<()>, Status> + Send + 'static,
+    /// Starts tokio thread collecting stats and sending them to backend service via gRPC.
+    fn spawn_stats_thread(
+        &self,
+        client: Arc<
+            Mutex<
+                GatewayServiceClient<
+                    InterceptedService<
+                        Channel,
+                        impl Fn(Request<()>) -> Result<Request<()>, Status> + Send + 'static,
+                    >,
                 >,
             >,
         >,
-    >,
-) -> Result<Streaming<Update>, GatewayError> {
-    loop {
-        debug!("Connecting to Defguard GRPC endpoint: {}", config.grpc_url);
-        let (response, stream) = {
+    ) {
+        // Create an async stream that periodically yields wireguard interface statistics.
+        info!("Spawning stats thread");
+        let period = Duration::from_secs(self.config.stats_period);
+        let ifname = self.config.ifname.clone();
+        let userspace = self.config.userspace;
+        let stats_stream = async_stream::stream! {
+            let api = WGApi::new(ifname, userspace);
+            loop {
+                debug!("Sending peer stats update");
+                match api.read_host() {
+                    Ok(host) => {
+                        for peer in host
+                            .peers
+                            .values()
+                            .filter(|p| p.last_handshake.map_or(
+                                false,
+                                |lhs| lhs != SystemTime::UNIX_EPOCH)
+                            ) {
+                            yield peer.into();
+                        }
+                    },
+                    Err(err) => error!("Failed to retrieve WireGuard interface stats {}", err),
+                }
+                sleep(period).await;
+            }
+        };
+        // Spawn the thread
+        tokio::spawn(async move {
             let mut client = client.lock().await;
-            let response = client
-                .config(ConfigurationRequest {
-                    name: config.name.clone(),
-                })
-                .await;
-            let stream = client.updates(()).await;
-            (response, stream)
-        };
-        match (response, stream) {
-            (Ok(response), Ok(stream)) => {
-                configure(config, response.into_inner())?;
-                spawn_stats_thread(
-                    Arc::clone(&client),
-                    Duration::from_secs(config.stats_period),
-                    config.ifname.clone(),
-                    config.userspace,
-                );
-                info!("Connected to Defguard GRPC endpoint: {}", config.grpc_url);
-                break Ok(stream.into_inner());
-            }
-            (Err(err), _) => {
-                error!("Couldn't retrieve gateway configuration, retrying: {}", err);
-            }
-            (_, Err(err)) => {
-                error!("Couldn't establish streaming connection, retrying: {}", err);
-            }
+            let _r = client.stats(Request::new(stats_stream)).await;
+        });
+    }
+
+    /// Performs complete interface reconfiguration based on `configuration` object.
+    /// Called when gateway (re)connects to GRPC endpoint and retrieves complete
+    /// network and peers data.
+    fn configure(&self, configuration: Configuration) -> Result<(), GatewayError> {
+        debug!(
+            "Received configuration, reconfiguring WireGuard interface: {:?}",
+            mask!(configuration, prvkey)
+        );
+        if !self.config.userspace {
+            #[cfg(target_os = "linux")]
+            let _ = delete_interface(&self.config.ifname);
         }
-        sleep(Duration::from_secs(1)).await;
-    }
-}
-
-/// Initialize logging to syslog.
-fn init_syslog(config: &Config, pid: u32) -> Result<(), GatewayError> {
-    let formatter = Formatter3164 {
-        facility: Facility::from_str(&config.syslog_facility).unwrap_or_default(),
-        hostname: None,
-        process: "defguard-gateway".into(),
-        pid,
-    };
-    let logger = syslog::unix_custom(formatter, &config.syslog_socket)?;
-    log::set_boxed_logger(Box::new(BasicLogger::new(logger)))?;
-    log::set_max_level(log::LevelFilter::Debug);
-    Ok(())
-}
-
-/// Starts the gateway process.
-/// * Retrieves configuration and configuration updates from Defguard GRPC server
-/// * Manages the interface according to configuration and updates
-/// * Sends interface statistics to Defguard server periodically
-pub async fn start(config: &Config) -> Result<(), GatewayError> {
-    let pid = process::id();
-
-    if let Some(pidfile) = &config.pidfile {
-        let mut file = File::create(pidfile)?;
-        file.write_all(pid.to_string().as_bytes())?;
+        setup_interface(&self.config.ifname, self.config.userspace, &configuration)?;
+        info!(
+            "Reconfigured WireGuard interface: {:?}",
+            mask!(configuration, prvkey)
+        );
+        Ok(())
     }
 
-    println!("Initializing logging facilities");
-    if config.use_syslog {
-        if let Err(error) = init_syslog(config, pid) {
-            eprintln!("Unable to initialize syslog. Is the syslog daemon running?");
-            return Err(error);
-        };
-    } else {
-        init_from_env(Env::default().filter_or(DEFAULT_FILTER_ENV, "info"));
-    }
-
-    info!(
-        "Starting Defguard gateway version {} with configuration: {:?}",
-        VERSION,
-        mask!(config, token)
-    );
-
-    let endpoint = Endpoint::from_shared(config.grpc_url.clone())?;
-    let endpoint = endpoint.http2_keep_alive_interval(Duration::from_secs(10));
-    let endpoint = endpoint.tcp_keepalive(Some(Duration::from_secs(10)));
-    let endpoint = if let Some(ca) = &config.grpc_ca {
-        let ca = std::fs::read_to_string(ca)?;
-        let tls = ClientTlsConfig::new().ca_certificate(Certificate::from_pem(&ca));
-        endpoint.tls_config(tls)?
-    } else {
-        endpoint
-    };
-    let channel = endpoint.connect_lazy();
-
-    let token = MetadataValue::try_from(&config.token)?;
-    let hostname = MetadataValue::from_static(&HOSTNAME);
-    let jwt_auth_interceptor = move |mut req: Request<()>| -> Result<Request<()>, Status> {
-        req.metadata_mut().insert("authorization", token.clone());
-        req.metadata_mut().insert("hostname", hostname.clone());
-        Ok(req)
-    };
-    let client = Arc::new(Mutex::new(GatewayServiceClient::with_interceptor(
-        channel,
-        jwt_auth_interceptor,
-    )));
-
-    let wgapi = WGApi::new(config.ifname.clone(), config.userspace);
-    let mut updates_stream = connect(config, Arc::clone(&client)).await?;
-    loop {
-        match updates_stream.message().await {
-            Ok(Some(update)) => {
-                debug!("Received update: {:?}", update);
-                match update.update {
-                    Some(update::Update::Network(configuration)) => {
-                        configure(config, configuration)?
-                    }
-                    Some(update::Update::Peer(peer_config)) => {
-                        info!("Applying peer configuration: {:?}", peer_config);
-                        match update.update_type {
-                            // UpdateType::Delete
-                            2 => wgapi.delete_peer(&peer_config.into()),
-                            // UpdateType::Create, UpdateType::Modify
-                            _ => wgapi.write_peer(&peer_config.into()),
-                        }?
-                    }
-                    _ => warn!("Unsupported kind of update"),
+    /// Continuously tries to connect to GRPC endpoint. Once the connection is established
+    /// configures the interface, starts the stats thread, connects and returns the updates stream
+    async fn connect(
+        &self,
+        client: Arc<
+            Mutex<
+                GatewayServiceClient<
+                    InterceptedService<
+                        Channel,
+                        impl Fn(Request<()>) -> Result<Request<()>, Status> + Send + 'static,
+                    >,
+                >,
+            >,
+        >,
+    ) -> Result<Streaming<Update>, GatewayError> {
+        loop {
+            debug!(
+                "Connecting to Defguard GRPC endpoint: {}",
+                self.config.grpc_url
+            );
+            let (response, stream) = {
+                let mut client = client.lock().await;
+                let response = client
+                    .config(ConfigurationRequest {
+                        name: self.config.name.clone(),
+                    })
+                    .await;
+                let stream = client.updates(()).await;
+                (response, stream)
+            };
+            match (response, stream) {
+                (Ok(response), Ok(stream)) => {
+                    self.configure(response.into_inner())?;
+                    self.spawn_stats_thread(client.clone());
+                    info!(
+                        "Connected to Defguard GRPC endpoint: {}",
+                        self.config.grpc_url
+                    );
+                    break Ok(stream.into_inner());
+                }
+                (Err(err), _) => {
+                    error!("Couldn't retrieve gateway configuration, retrying: {}", err);
+                }
+                (_, Err(err)) => {
+                    error!("Couldn't establish streaming connection, retrying: {}", err);
                 }
             }
-            Ok(None) => {
-                warn!("Received empty message, reconnecting");
-                updates_stream = connect(config, Arc::clone(&client)).await?;
-            }
-            Err(err) => {
-                error!("Server error {err}, reconnecting");
-                updates_stream = connect(config, Arc::clone(&client)).await?;
+            sleep(Duration::from_secs(1)).await;
+        }
+    }
+
+    /// Starts the gateway process.
+    /// * Retrieves configuration and configuration updates from Defguard GRPC server
+    /// * Manages the interface according to configuration and updates
+    /// * Sends interface statistics to Defguard server periodically
+    pub async fn start(&self) -> Result<(), GatewayError> {
+        info!(
+            "Starting Defguard gateway version {} with configuration: {:?}",
+            VERSION,
+            mask!(self.config, token)
+        );
+
+        debug!("Setting up gRPC server connection");
+        let endpoint = Endpoint::from_shared(self.config.grpc_url.clone())?;
+        let endpoint = endpoint.http2_keep_alive_interval(Duration::from_secs(10));
+        let endpoint = endpoint.tcp_keepalive(Some(Duration::from_secs(10)));
+        let endpoint = if let Some(ca) = &self.config.grpc_ca {
+            let ca = std::fs::read_to_string(ca)?;
+            let tls = ClientTlsConfig::new().ca_certificate(Certificate::from_pem(&ca));
+            endpoint.tls_config(tls)?
+        } else {
+            endpoint
+        };
+        let channel = endpoint.connect_lazy();
+
+        let token = MetadataValue::try_from(&self.config.token)?;
+        let hostname = MetadataValue::from_static(&HOSTNAME);
+        let jwt_auth_interceptor = move |mut req: Request<()>| -> Result<Request<()>, Status> {
+            req.metadata_mut().insert("authorization", token.clone());
+            req.metadata_mut().insert("hostname", hostname.clone());
+            Ok(req)
+        };
+        let client = Arc::new(Mutex::new(GatewayServiceClient::with_interceptor(
+            channel,
+            jwt_auth_interceptor,
+        )));
+
+        let wgapi = WGApi::new(self.config.ifname.clone(), self.config.userspace);
+        let mut updates_stream = self.connect(Arc::clone(&client)).await?;
+        loop {
+            match updates_stream.message().await {
+                Ok(Some(update)) => {
+                    debug!("Received update: {:?}", update);
+                    match update.update {
+                        Some(update::Update::Network(configuration)) => {
+                            self.configure(configuration)?
+                        }
+                        Some(update::Update::Peer(peer_config)) => {
+                            info!("Applying peer configuration: {:?}", peer_config);
+                            match update.update_type {
+                                // UpdateType::Delete
+                                2 => wgapi.delete_peer(&peer_config.into()),
+                                // UpdateType::Create, UpdateType::Modify
+                                _ => wgapi.write_peer(&peer_config.into()),
+                            }?
+                        }
+                        _ => warn!("Unsupported kind of update"),
+                    }
+                }
+                Ok(None) => {
+                    warn!("Received empty message, reconnecting");
+                    updates_stream = self.connect(Arc::clone(&client)).await?;
+                }
+                Err(err) => {
+                    error!("Server error {err}, reconnecting");
+                    updates_stream = self.connect(Arc::clone(&client)).await?;
+                }
             }
         }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::derive_partial_eq_without_eq)]
 
 pub mod config;
-mod error;
+pub mod error;
 pub mod gateway;
 pub mod wireguard;
 
@@ -11,6 +11,10 @@ pub mod proto {
 
 #[macro_use]
 extern crate log;
+
+use crate::{config::Config, error::GatewayError};
+use std::str::FromStr;
+use syslog::{BasicLogger, Facility, Formatter3164};
 
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -23,4 +27,18 @@ macro_rules! mask {
         object.$field = String::from("***");
         object
     }};
+}
+
+/// Initialize logging to syslog.
+pub fn init_syslog(config: &Config, pid: u32) -> Result<(), GatewayError> {
+    let formatter = Formatter3164 {
+        facility: Facility::from_str(&config.syslog_facility).unwrap_or_default(),
+        hostname: None,
+        process: "defguard-gateway".into(),
+        pid,
+    };
+    let logger = syslog::unix_custom(formatter, &config.syslog_socket)?;
+    log::set_boxed_logger(Box::new(BasicLogger::new(logger)))?;
+    log::set_max_level(log::LevelFilter::Debug);
+    Ok(())
 }
