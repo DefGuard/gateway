30810c214ae2307af7fbd258267c4acf241fb6b4
Maciek <mwojcik@teonite.com>
2023-12-19 13:13:36 +0100

    chore: update wireguard rs (#70)
    * update proto submodule
    
    * update gitignore
    
    * update dependencies
    
    * bump version
    
    ---------
    
    Co-authored-by: Maciej Wójcik <wojcik91@gmail.com>

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // compiling protos using path on build time
     let mut config = prost_build::Config::new();
+    // enable optional fields
     config.protoc_arg("--experimental_allow_proto3_optional");
     tonic_build::configure().compile_with_config(
         config,
