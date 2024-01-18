e5de40c3c68a32bd2923de656ea146d0668546ac
Adam <aciarcinski@teonite.com>
2024-01-18 16:56:51 +0100

    fix: do not fail on network interface creation (#76)

--- src/lib.rs
+++ src/lib.rs
@@ -102,7 +102,7 @@ impl From<proto::Peer> for Peer {
 
 impl From<&Peer> for proto::Peer {
     fn from(peer: &Peer) -> Self {
-        let preshared_key = peer.preshared_key.as_ref().map(|key| key.to_string());
+        let preshared_key = peer.preshared_key.as_ref().map(ToString::to_string);
         Self {
             pubkey: peer.public_key.to_string(),
             allowed_ips: peer.allowed_ips.iter().map(ToString::to_string).collect(),
