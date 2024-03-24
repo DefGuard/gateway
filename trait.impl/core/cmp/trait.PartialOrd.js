94f509e3c806fb80064d6bf9441610286f4bee74
teon <teon@users.noreply.github.com>
2024-03-24 17:35:34 +0000

    deploy: 85095f4dde74e33fda6212f97c171a4e62e48624

---
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"defguard_gateway":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"enum\" href=\"defguard_gateway/proto/enum.UpdateType.html\" title=\"enum defguard_gateway::proto::UpdateType\">UpdateType</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
