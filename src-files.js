499059957d2cfe1a0f3e22b5d3acfdcafc94ebac
t-aleksander <t-aleksander@users.noreply.github.com>
2024-08-30 09:06:18 +0000

    deploy: 5ab9cfc39270899388086cd8d73b7f7c32b4e0c3

---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["defguard_gateway",["",[],["config.rs","error.rs","gateway.rs","lib.rs","server.rs"]]]\
+]'));
+createSrcSidebar();
---
+++ trait.impl/clap_builder/derive/trait.CommandFactory.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"defguard_gateway":[["impl CommandFactory for <a class=\"struct\" href=\"defguard_gateway/config/struct.Config.html\" title=\"struct defguard_gateway::config::Config\">Config</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
