3aaaf60272ecc116b61dc8d104d8349dd6719c81
Maciek <19913370+wojcik91@users.noreply.github.com>
2024-11-13 15:08:08 +0100

    Add explicit missing CA file error (#135)
    * update gitignore
    
    * add path options typing
    
    * update dev flake
    
    * make io-related errors clearer
    
    * fix log content
    
    ---------
    
    Co-authored-by: Maciej Wójcik <maciek@wjck.pl>

--- flake.nix
+++ flake.nix
@@ -2,13 +2,30 @@
   description = "Rust development flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
   };
 
-  outputs = { nixpkgs, utils, ... }:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {
+        inherit system overlays;
+      };
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = ["rust-analyzer" "rust-src" "rustfmt" "clippy"];
+      };
     in {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
@@ -16,10 +33,7 @@
           openssl
           protobuf
           sqlx-cli
-          cargo
-          rustc
-          rust-analyzer
-          clippy
+          rustToolchain
         ];
       };
     });
