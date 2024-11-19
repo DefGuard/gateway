383fc20165c36e96502fca4fb7b7a1b38d34fc9e
t-aleksander <t-aleksander@users.noreply.github.com>
2024-11-19 13:25:32 +0000

    deploy: 4fdb26d4a4867ce7a40e3ecb9c3dfb1ec2259a83

---
+++ trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["defguard_gateway",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.82.0/std/primitive.i32.html\">i32</a>&gt; for <a class=\"enum\" href=\"defguard_gateway/proto/enum.UpdateType.html\" title=\"enum defguard_gateway::proto::UpdateType\">UpdateType</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[419]}
