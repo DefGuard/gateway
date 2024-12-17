2e67f9fe05f05510c2e7ec5527d15e5d61d71668
Adam Ciarciński <aciarcinski@teonite.com>
2024-12-17 12:20:29 +0100

    Fix OPNsense plugin

--- opnsense/src/opnsense/mvc/app/controllers/OPNsense/DefguardGateway/Api/ServiceController.php
+++ opnsense/src/opnsense/mvc/app/controllers/OPNsense/DefguardGateway/Api/ServiceController.php
@@ -1,37 +1,17 @@
 <?php
 namespace OPNsense\DefguardGateway\Api;
 
-use OPNsense\Base\ApiControllerBase;
+use OPNsense\Base\ApiMutableServiceControllerBase;
 use OPNsense\Core\Backend;
 
 /**
  * Class ServiceController
  * @package OPNsense\DefguardGateway
  */
-class ServiceController extends ApiControllerBase
+class ServiceController extends ApiMutableServiceControllerBase
 {
-    /**
-     * reconfigure DefguardGateway
-     */
-
-    public function reloadAction()
-    {
-        $status = "failed";
-        if ($this->request->isPost()) {
-            $backend = new Backend();
-            $bckresult = trim(
-                $backend->configdRun("template reload OPNsense/DefguardGateway")
-            );
-            if ($bckresult == "OK") {
-                $status = "Configuration saved to /etc/defguard/gateway.toml";
-            }
-        }
-        return ["status" => $status];
-    }
-    public function resetAction()
-    {
-        $backend = new Backend();
-        $result = trim($backend->configdRun("defguardgateway restart"));
-        return ["status" => $result];
-    }
+    protected static $internalServiceName = "defguardgateway";
+    protected static $internalServiceClass = "\OPNsense\DefguardGateway\DefguardGateway";
+    protected static $internalServiceTemplate = "OPNsense/DefguardGateway";
+    protected static $internalServiceEnabled = "general.Enabled";
 }
--- opnsense/src/opnsense/mvc/app/controllers/OPNsense/DefguardGateway/Api/SettingsController.php
+++ opnsense/src/opnsense/mvc/app/controllers/OPNsense/DefguardGateway/Api/SettingsController.php
@@ -1,55 +1,57 @@
 <?php
 namespace OPNsense\DefguardGateway\Api;
 
-use OPNsense\Base\ApiControllerBase;
-use OPNsense\DefguardGateway\DefguardGateway;
+use OPNsense\Base\ApiMutableModelControllerBase;
 use OPNsense\Core\Config;
+
 /**
  * Class SettingsController Handles settings related API actions for the DefguardGateway module
  * @package OPNsense\DefguardGateway
  */
-
-class SettingsController extends ApiControllerBase
+class SettingsController extends ApiMutableModelControllerBase
 {
-    public function getAction()
-    {
-        // define list of configurable settings
-        $result = [];
-        if ($this->request->isGet()) {
-            $mdlDefguardGateway = new DefguardGateway();
-            $result["defguardgateway"] = $mdlDefguardGateway->getNodes();
-        }
-        return $result;
-    }
+    protected static $internalModelClass = "\OPNsense\DefguardGateway\DefguardGateway";
+    protected static $internalModelName = "defguardgateway";
 
-    public function setAction()
-    {
-        $result = ["result" => "failed"];
-        if ($this->request->isPost()) {
-            // load model and update with provided data
-            $mdlDefguardGateway = new DefguardGateway();
-            $mdlDefguardGateway->setNodes(
-                $this->request->getPost("defguardgateway")
-            );
+    // public function getAction()
+    // {
+    //     // define list of configurable settings
+    //     $result = [];
+    //     if ($this->request->isGet()) {
+    //         $mdlDefguardGateway = new DefguardGateway();
+    //         $result["defguardgateway"] = $mdlDefguardGateway->getNodes();
+    //     }
+    //     return $result;
+    // }
 
-            // perform validation
-            $valMsgs = $mdlDefguardGateway->performValidation();
-            foreach ($valMsgs as $field => $msg) {
-                if (!array_key_exists("validations", $result)) {
-                    $result["validations"] = [];
-                }
-                $result["validations"][
-                    "defguardgateway." . $msg->getField()
-                ] = $msg->getMessage();
-            }
+    // public function setAction()
+    // {
+    //     $result = ["result" => "failed"];
+    //     if ($this->request->isPost()) {
+    //         // load model and update with provided data
+    //         $mdlDefguardGateway = new DefguardGateway();
+    //         $mdlDefguardGateway->setNodes(
+    //             $this->request->getPost("defguardgateway")
+    //         );
 
-            // serialize model to config and save
-            if ($valMsgs->count() == 0) {
-                $mdlDefguardGateway->serializeToConfig();
-                Config::getInstance()->save();
-                $result["result"] = "saved";
-            }
-        }
-        return $result;
-    }
+    //         // perform validation
+    //         $valMsgs = $mdlDefguardGateway->performValidation();
+    //         foreach ($valMsgs as $field => $msg) {
+    //             if (!array_key_exists("validations", $result)) {
+    //                 $result["validations"] = [];
+    //             }
+    //             $result["validations"][
+    //                 "defguardgateway." . $msg->getField()
+    //             ] = $msg->getMessage();
+    //         }
+
+    //         // serialize model to config and save
+    //         if ($valMsgs->count() == 0) {
+    //             $mdlDefguardGateway->serializeToConfig();
+    //             Config::getInstance()->save();
+    //             $result["result"] = "saved";
+    //         }
+    //     }
+    //     return $result;
+    // }
 }
