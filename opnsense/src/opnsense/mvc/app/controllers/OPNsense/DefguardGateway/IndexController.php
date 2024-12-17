39fff1b822c5fda72a0d306064d32ae51bab7f5d
Adam <aciarcinski@teonite.com>
2024-12-17 12:55:53 +0100

    Handle config files properly (#141)

--- opnsense/src/opnsense/mvc/app/controllers/OPNsense/DefguardGateway/IndexController.php
+++ opnsense/src/opnsense/mvc/app/controllers/OPNsense/DefguardGateway/IndexController.php
@@ -4,9 +4,7 @@ class IndexController extends \OPNsense\Base\IndexController
 {
     public function indexAction()
     {
-        // pick the template to serve to our users.
         $this->view->pick("OPNsense/DefguardGateway/index");
-        // fetch form data "general"
         $this->view->generalForm = $this->getForm("general");
     }
 }
